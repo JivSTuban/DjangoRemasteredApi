24002bb34b9871940b85678df0227a3a59caa64b
jiv <jeremiatuban@Jivs-iMac.local>
2024-08-31 03:05:30 +0800

    sleept

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Blogpost
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = Blogpost.objects.all()
+    serializer_class = BlogPostSerializer
