c9b8aaae2e77d00dc2e4b1941941662d024eae08
jiv <jeremiatuban@Jivs-iMac.local>
2024-08-31 02:58:18 +0800

    first

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    publishedDate = models.DateTimeField(auto_add_now=True)
+
+    def __str__(self):
+        return self.title
